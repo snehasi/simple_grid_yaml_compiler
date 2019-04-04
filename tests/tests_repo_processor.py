abd278a8f3bc51a0d795d82d85e6d9d6b839b3af
snehasi <sneha16098@iiitd.ac.in>
2019-04-04 16:18:07 +0530

    Added test for compiler.repo_processor.analyse_repo_url

--- tests/tests_repo_processor.py
+++ tests/tests_repo_processor.py
@@ -1,2 +1,14 @@
-from compiler.repo_processor import get_default_values
+from compiler.repo_processor import analyse_repo_url
 import unittest
+
+class RepoProcessorTest(unittest.TestCase):
+	def test_analyse_repo_url(self):
+		repo_url = "https://github.com/WLCG-Lightweight-Sites/simple_grid_yaml_compiler"
+		
+		expected_output = {
+			"org_name": "WLCG-Lightweight-Sites",
+			"repo_name": "simple_grid_yaml_compiler",
+			"branch_name": "master"
+		}
+
+		self.assertDictEqual(analyse_repo_url(repo_url), expected_output)
