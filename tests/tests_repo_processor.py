fe5440b10c6b13037164692c4b14a84a664b95a3
snehasi <sneha16098@iiitd.ac.in>
2019-04-04 16:31:06 +0530

    Added tests for repo_processor generate file name functions

--- tests/tests_repo_processor.py
+++ tests/tests_repo_processor.py
@@ -1,4 +1,4 @@
-from compiler.repo_processor import analyse_repo_url
+from compiler.repo_processor import analyse_repo_url, generate_default_file_name, generate_config_schema_file_name, generate_meta_info_file_name
 import unittest
 
 class RepoProcessorTest(unittest.TestCase):
@@ -12,3 +12,36 @@ class RepoProcessorTest(unittest.TestCase):
 		}
 
 		self.assertDictEqual(analyse_repo_url(repo_url), expected_output)
+
+	def test_generate_default_file_name(self):
+		repo_info = {
+			"org_name": "WLCG-Lightweight-Sites",
+			"repo_name": "simple_grid_yaml_compiler",
+			"branch_name": "master"
+		}
+
+		expected_output = "./.temp/simple_grid_yaml_compiler_defaults.yaml"
+
+		self.assertEqual(generate_default_file_name(repo_info), expected_output)
+
+	def test_generate_config_schema_file_name(self):
+		repo_info = {
+			"org_name": "WLCG-Lightweight-Sites",
+			"repo_name": "simple_grid_yaml_compiler",
+			"branch_name": "master"
+		}
+
+		expected_output = "./.temp/simple_grid_yaml_compiler_schema.yaml"
+
+		self.assertEqual(generate_config_schema_file_name(repo_info), expected_output)
+
+	def test_generate_meta_info_file_name(self):
+		repo_info = {
+			"org_name": "WLCG-Lightweight-Sites",
+			"repo_name": "simple_grid_yaml_compiler",
+			"branch_name": "master"
+		}
+
+		expected_output = "./.temp/simple_grid_yaml_compiler_info.yaml"
+
+		self.assertEqual(generate_meta_info_file_name(repo_info), expected_output)
