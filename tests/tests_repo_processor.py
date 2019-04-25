9509c91b33638aa0801401cd89dfba790046e739
Sneha Sinha <sneha16098@iiitd.ac.in>
2019-04-25 09:22:20 +0530

    Revert "Added tests for repo_processor.get_config_schema"
    This reverts commit a45048c3d321e9ae5ccceec1a558deb145c96c0c.

--- tests/tests_repo_processor.py
+++ tests/tests_repo_processor.py
@@ -1,4 +1,4 @@
-from compiler.repo_processor import analyse_repo_url, generate_default_file_name, generate_config_schema_file_name, generate_meta_info_file_name, get_default_values, get_config_schema
+from compiler.repo_processor import analyse_repo_url, generate_default_file_name, generate_config_schema_file_name, generate_meta_info_file_name, get_default_values
 
 from os import mkdir
 from shutil import rmtree
@@ -61,20 +61,6 @@ class RepoProcessorTest(unittest.TestCase):
 			output = file.read()
 
 		file_url        = "https://raw.githubusercontent.com/WLCG-Lightweight-Sites/simple_grid_site_defaults/master/site_level_configuration_defaults.yaml"
-		expected_output = urlopen(file_url).read()
-
-		self.assertEqual(output, expected_output)
-
-	def test_get_config_schema(self):
-		repo_url = "https://github.com/WLCG-Lightweight-Sites/wlcg_lightweight_site_ce_cream"
-
-		repo_info = analyse_repo_url(repo_url)
-		fname     = generate_config_schema_file_name(repo_info)
-
-		with open(fname, "r") as file:
-			output = file.read()
-
-		file_url = "https://raw.githubusercontent.com/WLCG-Lightweight-Sites/wlcg_lightweight_site_ce_cream/master/config-schema.yaml"
 		expected_output = urlopen(file_url).read()
 
 		self.assertEqual(output, expected_output)
