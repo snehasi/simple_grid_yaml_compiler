75d3a7aa849254571706ec674dfd4ca96fa383e8
Sneha Sinha <sneha16098@iiitd.ac.in>
2019-04-25 09:22:20 +0530

    Revert "Added tests for repo_processor.get_default_values"
    This reverts commit 112d03f1ab8df1a4c84025aaaa92106672908a36.

--- tests/tests_repo_processor.py
+++ tests/tests_repo_processor.py
@@ -1,9 +1,4 @@
-from compiler.repo_processor import analyse_repo_url, generate_default_file_name, generate_config_schema_file_name, generate_meta_info_file_name, get_default_values
-
-from os import mkdir
-from shutil import rmtree
-from urllib2 import urlopen
-
+from compiler.repo_processor import analyse_repo_url, generate_default_file_name, generate_config_schema_file_name, generate_meta_info_file_name
 import unittest
 
 class RepoProcessorTest(unittest.TestCase):
@@ -50,17 +45,3 @@ class RepoProcessorTest(unittest.TestCase):
 		expected_output = "./.temp/simple_grid_yaml_compiler_info.yaml"
 
 		self.assertEqual(generate_meta_info_file_name(repo_info), expected_output)
-
-	def test_get_default_values(self):
-		repo_url      = "https://github.com/WLCG-Lightweight-Sites/simple_grid_site_defaults"
-		defaults_file = "site_level_configuration_defaults.yaml"
-
-		fname  = get_default_values(repo_url, defaults_file)
-
-		with open(fname, "r") as file:
-			output = file.read()
-
-		file_url        = "https://raw.githubusercontent.com/WLCG-Lightweight-Sites/simple_grid_site_defaults/master/site_level_configuration_defaults.yaml"
-		expected_output = urlopen(file_url).read()
-
-		self.assertEqual(output, expected_output)
